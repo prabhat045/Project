ff207ac643a93be86bbe4e1735608dc0858b15f4
prabhat045 <54550989+prabhat045@users.noreply.github.com>
2022-02-25 00:03:09 +0545

    forgetpassword integrated

---
+++ reactapp/src/Components/Signup/FormSignup.js
@@ -0,0 +1,90 @@
+import React from "react";
+import validate from "./validateInfo";
+import useForm from "./useForm";
+import "./Form.css";
+import { NavLink } from "react-router-dom";
+
+const FormSignup = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
+
+  return (
+    <div className='form-content-right'>
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h3>Welcome to FISH LANDIA..</h3>
+        <div className='form-inputs'>
+          <label className='form-label'>Username</label>
+          <input
+            className='form-input'
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+            value={values.username}
+            onChange={handleChange}
+          />
+          {errors.username && <p>{errors.username}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Email</label>
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            placeholder='Enter your email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+
+        <div className='form-inputs'>
+          <label className='form-label'>Mobile Number</label>
+          <input
+            className='form-input'
+            type='text'
+            name='number'
+            placeholder='Enter your Mobile Number'
+            value={values.mobileNo}
+            onChange={handleChange}
+          />
+          {errors.mobileNo && <p>{errors.mobileNo}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Confirm Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password2'
+            placeholder='Confirm your password'
+            value={values.password2}
+            onChange={handleChange}
+          />
+          {errors.password2 && <p>{errors.password2}</p>}
+        </div>
+        <button className='form-input-btn' type='submit'>
+          Sign up
+        </button>
+        <span className='form-input-login'>
+          Already have an account?
+          <NavLink to='/adminlogin'>Login here</NavLink>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default FormSignup;
